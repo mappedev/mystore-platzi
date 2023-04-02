517412a06dc62626b66b96e66c1728657b3264e8
mappedev <mappedev@gmail.com>
2023-04-02 10:26:13 -0400

    fix migrations order

--- src/db/migrations/20230401180152-create-order.js
+++ src/db/migrations/20230401180152-create-order.js
@@ -1,11 +1,39 @@
 'use strict';
 
-const { ORDER_TABLE_NAME, OrderSchema } = require('../models/orders');
+const { DataTypes, Sequelize } = require('sequelize');
+
+const { ORDER_TABLE_NAME } = require('../models/orders');
+const { CUSTOMER_TABLE_NAME } = require('../models/customers')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE_NAME, OrderSchema)
+    await queryInterface.createTable(ORDER_TABLE_NAME, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      // Associations
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        References: {
+          model: CUSTOMER_TABLE_NAME,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    })
   },
 
   async down(queryInterface) {
